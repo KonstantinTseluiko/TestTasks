b4a5c76034fade94813f110d1db15f7d90e6e039
Konstantin Tseluiko <celujkokostya@gmail.com>
2020-11-12 00:34:49 +0300

    tasks

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/CannotBuildPyramidException.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/CannotBuildPyramidException.java
@@ -1,4 +1,22 @@
 package com.tsystems.javaschool.tasks.pyramid;
 
 public class CannotBuildPyramidException extends RuntimeException {
+    public CannotBuildPyramidException() {
+    }
+
+    public CannotBuildPyramidException(String message) {
+        super(message);
+    }
+
+    public CannotBuildPyramidException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CannotBuildPyramidException(Throwable cause) {
+        super(cause);
+    }
+
+    public CannotBuildPyramidException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -1,6 +1,6 @@
 package com.tsystems.javaschool.tasks.pyramid;
 
-import java.util.List;
+import java.util.*;
 
 public class PyramidBuilder {
 
@@ -12,10 +12,54 @@ public class PyramidBuilder {
      * @return 2d array with pyramid inside
      * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
      */
-    public int[][] buildPyramid(List<Integer> inputNumbers) {
+    public int[][] buildPyramid(List<Integer> inputNumbers) throws CannotBuildPyramidException {
         // TODO : Implement your solution here
-        return new int[0][0];
+        if(inputNumbers.contains(null)){
+            throw new CannotBuildPyramidException();
+        }
+        Set<Integer> ts = new TreeSet<>(inputNumbers);
+        if(ts.size() == 1){
+            throw new CannotBuildPyramidException();
+        }
+        Iterator<Integer> iterator = ts.iterator();
+        int n = 1;
+        while ((2 * ts.size()) != 2 * n + n * (n - 1)) {
+            n++;
+            if(n>ts.size()){
+                throw new CannotBuildPyramidException();
+            }
+        }
+        System.out.println(n);
+        int sizeRow = n;
+        int sizeColumn = 2*n - 1;
+        int[][] arr = new int[sizeRow][sizeColumn];
+
+
+        for (int i = 0; i < sizeRow; i++) {
+            for (int j = 0; j < sizeColumn; j++) {
+                arr[i][j] = 0;
+            }
+        }
+        arr[0][sizeColumn / 2] = iterator.next();
+        for (int i = 0; i < sizeRow - 1; i++) {
+            for (int j = 0; j < sizeColumn; j++) {
+                if (arr[i][j] != 0 || (j == sizeColumn/2 && i == 0)) {
+                    if (arr[i + 1][j - 1] == 0) {
+                        arr[i + 1][j - 1] = iterator.next();
+                    }
+                    if (arr[i + 1][j + 1] == 0) {
+                        arr[i + 1][j + 1] = iterator.next();
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < sizeRow; i++) {
+            for (int j = 0; j < sizeColumn; j++) {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+        return arr;
     }
-
-
 }
